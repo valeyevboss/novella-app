f3f67441b00ce1461645a311b4c442a05bcb66de
valeyevboss <nvaleyevboss@gmail.com>
2024-09-08 00:27:37 +0300

    update tokens

--- public/app.js
+++ public/app.js
@@ -2,23 +2,23 @@ require('dotenv').config({ path: path.join(__dirname, '../telegram-bot-server/.e
 
 // Проверка статуса пользователя
 app.get('/check-status', async (req, res) => {
-    const telegramId = req.query.telegramId; // Получаем telegramId из запроса
-
+    const telegramId = req.query.telegramId;
     try {
         const user = await User.findOne({ telegramId: telegramId });
-
         if (user) {
-            res.json({ status: user.status }); // Возвращаем статус пользователя
+            res.json({ status: user.status });
         } else {
-            res.json({ status: 'No user found' }); // Если пользователь не найден
+            res.json({ status: 'No user found' });
         }
     } catch (err) {
         console.error(err);
-        res.status(500).json({ error: 'Server error' }); // Обработка ошибок
+        res.status(500).json({ error: 'Server error' });
     }
 });
 
-// Маршрут для страницы бана
+
+// Если пользователь забанен, перенаправляем его на banned.html
 app.get('/banned', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'public', 'banned.html'));
 });
+
