077827fd886a2f8d21c0bacefb815b444ac817db
valeyevboss <nvaleyevboss@gmail.com>
2024-09-13 03:22:39 +0300

    update database

--- models/BlockedUser.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const BlockedUserSchema = new mongoose.Schema({
-    telegramId: Number,
-    reason: String,
-    bannedAt: Date
-});
-
-module.exports = mongoose.model('BlockedUser', BlockedUserSchema);
