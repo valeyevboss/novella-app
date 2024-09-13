c9e034f1112e5ff9d97cdc178405cf35f09dd53a
valeyevboss <nvaleyevboss@gmail.com>
2024-09-13 03:31:10 +0300

    update database

--- telegram-bot-server/server.js
+++ telegram-bot-server/server.js
@@ -36,8 +36,6 @@ app.get('/loadingerror', (req, res) => {
 async function startServer() {
     try {
         await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
         });
         console.log('Connected to MongoDB');
 
