e43b72ac0ffb15b2d38f4be97e09aacf867de68b
valeyevboss <nvaleyevboss@gmail.com>
2024-09-19 00:33:45 +0300

    Update balance

--- telegram-bot-server/server.js
+++ telegram-bot-server/server.js
@@ -89,6 +89,21 @@ app.get('/check-user/:telegramId', async (req, res) => {
     }
 });
 
+// Маршрут для получения баланса пользователя по Telegram ID
+app.get('/api/get-balance/:telegramId', async (req, res) => {
+    const telegramId = req.params.telegramId;
+    
+    // модель User для работы с базой данных
+    const user = await User.findOne({ telegramId: telegramId });
+
+    if (!user) {
+        return res.status(404).json({ error: 'User not found' });
+    }
+
+    res.json({ tokens: user.tokens });
+});
+
+
 // Объявляем URL изображения
 const imageUrl = 'https://res.cloudinary.com/dvjohgg6j/image/upload/v1725631955/Banner/Novella%20banner.jpg'; // Публичный URL вашего изображения
 
