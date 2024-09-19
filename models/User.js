db3de33539b49b9f89b74331fcde13a8a04e4090
valeyevboss <nvaleyevboss@gmail.com>
2024-09-19 16:56:41 +0300

    Update modules scripts

--- models/User.js
+++ models/User.js
@@ -1,17 +1,16 @@
 const mongoose = require('mongoose');
-const { v4: uuidv4 } = require('uuid');  // Для генерации уникальных userID
 
 const UserSchema = new mongoose.Schema({
     telegramId: { type: String, unique: true },
-    userID: { type: String, unique: true, default: uuidv4 },  // Генерируем уникальный userID
     username: { type: String, default: '' },
     lastLogin: Date,
     tokens: { type: Number, default: 0 },
     status: { type: String, enum: ['No banned', 'banned'], default: 'No banned' },
+	//rank: { type: Number, default: 0 },  // Поле для ранга
+	//avatarUrl: { type: String, default: '' }  // Поле для URL аватара
 });
 
-// Добавляем индексы на поля telegramId и userID
+// Добавляем уникальный индекс на поле telegramId
 UserSchema.index({ telegramId: 1 }, { unique: true });
-UserSchema.index({ userID: 1 }, { unique: true });
 
 module.exports = mongoose.model('User', UserSchema);
