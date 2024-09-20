a8146957a711d84d5060f07b227361f76dddd72d
valeyevboss <nvaleyevboss@gmail.com>
2024-09-20 18:23:52 +0300

    add method tokens update v7

--- models/User.js
+++ models/User.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-const { v4: uuidv4 } = require('uuid');  // Используем UUID для генерации уникальных идентификаторов
 
 const UserSchema = new mongoose.Schema({
     telegramId: { type: String, unique: true },
@@ -10,8 +9,7 @@ const UserSchema = new mongoose.Schema({
     status: { type: String, enum: ['No banned', 'banned'], default: 'No banned' },
 });
 
-// Добавляем уникальные индексы на поля telegramId и userId
+// Добавляем уникальные индексы на поля telegramId
 UserSchema.index({ telegramId: 1 }, { unique: true });
-UserSchema.index({ userId: 1 }, { unique: true });
 
 module.exports = mongoose.model('User', UserSchema);
