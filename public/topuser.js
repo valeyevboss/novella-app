b9a68a5bcd205f766721276ad1180fa50034f142
valeyevboss <nvaleyevboss@gmail.com>
2024-10-01 18:43:03 +0300

    update leaderboard pin + styles uncorrect v6

--- public/topuser.js
+++ public/topuser.js
@@ -16,11 +16,10 @@ function displayTopUsers(users) {
     const leaderboardContainer = document.createElement('div');
     leaderboardContainer.classList.add('top100-container');
 
-    // Создаем общий блок для пользователей
-    const userBlock = document.createElement('div');
-    userBlock.classList.add('top100-user-info-block');
-
     users.forEach((user, index) => {
+        const userBlock = document.createElement('div');
+        userBlock.classList.add('top100-user-info-block');
+
         // Проверка на наличие аватарки, если нет — использовать дефолтную
         const avatarUrl = user.avatarUrl ? user.avatarUrl : 'https://res.cloudinary.com/dvjohgg6j/image/upload/v1727453958/default-avatar.png';
 
@@ -34,10 +33,7 @@ function displayTopUsers(users) {
             rankIcon = '<img src="https://res.cloudinary.com/dvjohgg6j/image/upload/v1727725289/Pin/Bronze-pin.png" alt="Top 3" class="rank-icon">';
         }
 
-        // Добавляем каждого пользователя в общий блок
-        const userEntry = document.createElement('div');
-        userEntry.classList.add('top100-user-entry'); // Класс для отдельных записей
-        userEntry.innerHTML = `
+        userBlock.innerHTML = `
             <img src="${avatarUrl}" alt="User Avatar" class="top100-user-avatar">
             <div class="top100-user-details">
                 <span class="top100-username">${user.username}</span>
@@ -46,10 +42,9 @@ function displayTopUsers(users) {
             <span class="top100-user-rank">${rankIcon} #${index + 1}</span>
         `;
 
-        userBlock.appendChild(userEntry);
+        leaderboardContainer.appendChild(userBlock);
     });
 
-    leaderboardContainer.appendChild(userBlock);
     document.body.appendChild(leaderboardContainer);
 }
 
