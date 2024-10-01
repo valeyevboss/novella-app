7affe05e5399922d2662fb3588978832dc5cc337
valeyevboss <nvaleyevboss@gmail.com>
2024-10-01 18:54:46 +0300

    update leaderboard pin + styles uncorrect v6

--- public/topuser.js
+++ public/topuser.js
@@ -17,9 +17,6 @@ function displayTopUsers(users) {
     leaderboardContainer.classList.add('top100-container');
 
     users.forEach((user, index) => {
-        const userBlock = document.createElement('div');
-        userBlock.classList.add('top100-user-info-block');
-
         // Проверка на наличие аватарки, если нет — использовать дефолтную
         const avatarUrl = user.avatarUrl ? user.avatarUrl : 'https://res.cloudinary.com/dvjohgg6j/image/upload/v1727453958/default-avatar.png';
 
@@ -33,11 +30,18 @@ function displayTopUsers(users) {
             rankIcon = '<img src="https://res.cloudinary.com/dvjohgg6j/image/upload/v1727725289/Pin/Bronze-pin.png" alt="Top 3" class="rank-icon">';
         }
 
+        // Создаем блок для каждого пользователя
+        const userBlock = document.createElement('div');
+        userBlock.classList.add('top100-user-info-block');
+
         userBlock.innerHTML = `
             <img src="${avatarUrl}" alt="User Avatar" class="top100-user-avatar">
             <div class="top100-user-details">
                 <span class="top100-username">${user.username}</span>
+            </div>
+            <div class="top100-token-details">
                 <span class="top100-token-balance">${user.tokens}</span>
+                <img src="https://res.cloudinary.com/dvjohgg6j/image/upload/v1725622251/novellacoin.png" alt="Token Icon" class="top100-token-icon">
             </div>
             <span class="top100-user-rank">${rankIcon} #${index + 1}</span>
         `;
