7df45400e3abc996800d7eca28b669c73755bd52
valeyevboss <nvaleyevboss@gmail.com>
2024-10-20 18:57:26 +0300

    Game update v10

--- telegram-bot-server/server.js
+++ telegram-bot-server/server.js
@@ -399,7 +399,7 @@ app.listen(3000, () => {
 });
 
 // Обмен коинов на токены
-app.get('/exchange/:telegramId', async (req, res) => {
+app.post('/api/exchange/:telegramId', async (req, res) => {
     const { coinAmount, tokensReceived } = req.body;
 
     try {
